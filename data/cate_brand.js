2264e75feb24e8314b7f3216268d9076c81d6367
Tonyamano2727 <toanb3074@gmail.com>
2024-07-06 22:10:49 +0700

    update

---
+++ data/cate_brand.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        cate: 'Smartphone',
+        brand: ['Apple', 'Samsung']
+    },
+    {
+        cate: 'Tablet',
+        brand: ['Ipad', 'SamSung']
+    },
+    {
+        cate: 'Laptop',
+        brand: ['Asus', 'Dell','Acer','Lenovo','HP','Macbook']
+    },
+    {
+        cate: 'Speaker',
+        brand: ['Samsung','Apple']
+    },
+    {
+        cate: 'Headphone',
+        brand: ['Samsung','Apple']
+    },
+    {
+        cate: 'Accessories',
+        brand: ['Samsung','Apple','JBL']
+    },
+    {
+        cate: 'Television',
+        brand: ['Apple', 'Samsung','Asus']
+    },
+]
+module.exports = data
